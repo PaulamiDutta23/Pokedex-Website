a7d41a31c9f5bb689f7b61cf9367f4cdcf45b55f
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2026-03-04 18:24:23 +0530

    Changed to frontend managing

--- FE/public/scripts/event_handlers.js
+++ FE/public/scripts/event_handlers.js
@@ -1,22 +1,91 @@
+const fetchAllPokemon = () =>
+  fetch("./data/pokemons.json").then((d) => d.json());
+
 const capitalizeFirstLetter = (string) => {
   const type = string.toLowerCase();
   const firstLetter = type[0].toUpperCase();
   return firstLetter.concat(type.slice(1));
 };
 
-const renderPage = async (currentType, cardContainer) => {
-  const type = capitalizeFirstLetter(currentType || "all");
-  const data = await fetch("./data/pokemons.json").then((d) => d.json());
-  const pokemons = currentType ? data.filter((p) => p.types.includes(type)) : data;
-  const card = document.createElement("h2");
-  card.append(`${pokemons[0].pokeName}`);
-  cardContainer.append(card);
+const createFragments = ([tag, attrs, ...contents]) => {
+  const element = document.createElement(tag);
+  for (const [key, value] of Object.entries(attrs)) {
+    element.setAttribute(key, value);
+  }
+  if (contents.length === 1 && typeof contents[0] === "string") {
+    element.textContent = contents;
+    return element;
+  }
+  element.append(...contents.map((c) => createFragments(c)));
+  return element;
 };
 
-window.onload = () => {
+const createImg = ({ pokeName, id }) => [
+  "img",
+  {
+    src:
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+    alt: `This is an image of ${pokeName}`,
+  },
+  "",
+];
+
+const createName = ({ pokeName }) => ["h2", {}, pokeName];
+
+const createTypes = ({ types }) =>
+  types.map((type) => ["div", { class: `type ${type.toLowerCase()}` }, type]);
+
+const createRows = (
+  { weight, base_experience, hp, attack, defense, speed },
+) => {
+  const stats = {
+    "Weight": weight,
+    "Base XP": base_experience,
+    "HP": hp,
+    "Attack": attack,
+    "Defense": defense,
+    "Speed": speed,
+  };
+  const statsContainer = [];
+  for (const [key, value] of Object.entries(stats)) {
+    statsContainer.push(["tr", {}, ["td", { class: "data" }, key], ["td", {
+      class: "right-align",
+    }, value ? value.toString() : "Unknown"]]);
+  }
+  return statsContainer;
+};
+
+const createPokemonCard = (pokemon) => {
+  const img = ["div", { class: "img-container" }, createImg(pokemon)];
+  const header = [
+    "div",
+    { class: "header" },
+    createName(pokemon),
+    ["div", { class: "types" }, ...createTypes(pokemon)],
+  ];
+  const stats = ["table", { class: "stats" }, ...createRows(pokemon)];
+  return createFragments(["div", { class: "card" }, img, header, stats]);
+};
+
+const renderPage = (allPokemon, type, cardContainer) => {
+  const currentType = type ? capitalizeFirstLetter(type) : "All";
+  const pokemon = type
+    ? allPokemon.filter((p) => p.types.includes(currentType))
+    : allPokemon;
+  const cards = pokemon.map((pokemon, i) => {
+    return createPokemonCard(pokemon);
+  });
+
+  // console.log(...pokemon.map(createPokemonCard));
+  cardContainer.append(...pokemon.map(createPokemonCard));
+  // return cardContainer;
+};
+
+window.onload = async () => {
   const cardContainer = document.querySelector(".card-container");
+  const allPokemon = await fetchAllPokemon();
   const type = window.location.pathname.split("/").pop();
   fetch(`/${type}`).then((res) => res.text()).then((_data) =>
-    renderPage(type, cardContainer)
+    renderPage(allPokemon, type, cardContainer)
   );
 };
