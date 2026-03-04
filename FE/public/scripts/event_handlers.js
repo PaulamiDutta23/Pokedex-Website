1735eb5c7ecd493126371975d7f2f9fa69079c6c
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2026-03-05 00:36:01 +0530

    Refactors event_handler.js
    * changes one key name in json data

--- FE/public/scripts/event_handlers.js
+++ FE/public/scripts/event_handlers.js
@@ -1,5 +1,5 @@
 const fetchAllPokemon = () =>
-  fetch("./data/pokemons.json").then((d) => d.json());
+  fetch("./data/pokemons.json").then((data) => data.json());
 
 const capitalizeFirstLetter = (string) => {
   const type = string.toLowerCase();
@@ -7,17 +7,10 @@ const capitalizeFirstLetter = (string) => {
   return firstLetter.concat(type.slice(1));
 };
 
-const createFragments = ([tag, attrs, ...contents]) => {
-  const element = document.createElement(tag);
-  for (const [key, value] of Object.entries(attrs)) {
-    element.setAttribute(key, value);
-  }
-  if (contents.length === 1 && typeof contents[0] === "string") {
-    element.textContent = contents;
-    return element;
-  }
-  element.append(...contents.map((c) => createFragments(c)));
-  return element;
+const setBgStyle = ({ types }) => {
+  const gradinets = types.map((t) => `hsl(var(--${t.toLowerCase()}) / 50%)`)
+    .join(",");
+  return `background: linear-gradient(135deg,${gradinets} , transparent 85%);`;
 };
 
 const createImg = ({ pokeName, id }) => [
@@ -30,34 +23,37 @@ const createImg = ({ pokeName, id }) => [
   "",
 ];
 
-const createName = ({ pokeName }) => ["h2", {}, pokeName];
-
 const createTypes = ({ types }) =>
   types.map((type) => ["div", { class: `type ${type.toLowerCase()}` }, type]);
 
-const createRows = (
-  { weight, base_experience, hp, attack, defense, speed },
-) => {
+const createRows = ({ weight, base_xp, hp, attack, defense, speed }) => {
   const stats = {
     "Weight": weight,
-    "Base XP": base_experience,
+    "Base XP": base_xp,
     "HP": hp,
     "Attack": attack,
     "Defense": defense,
     "Speed": speed,
   };
-  const statsContainer = [];
-  for (const [key, value] of Object.entries(stats)) {
-    statsContainer.push(["tr", {}, ["td", { class: "data" }, key], ["td", {
-      class: "right-align",
-    }, value ? value.toString() : "Unknown"]]);
-  }
-  return statsContainer;
+
+  return Object.entries(stats).map((
+    [key, value],
+  ) => ["tr", {}, ["td", { class: "data" }, key], ["td", {
+    class: "right-align",
+  }, value ? value.toString() : "Unknown"]]);
 };
 
-const setBgStyle = ({ types }) => {
-  const gradinets = types.map((t) => `hsl(var(--${t.toLowerCase()}) / 50%)`).join(",");
-  return `background: linear-gradient(135deg,${gradinets} , transparent 85%);`;
+const createFragments = ([tag, attrs, ...contents]) => {
+  const element = document.createElement(tag);
+  for (const [key, value] of Object.entries(attrs)) {
+    element.setAttribute(key, value);
+  }
+  if (contents.length === 1 && typeof contents[0] === "string") {
+    element.textContent = contents;
+    return element;
+  }
+  element.append(...contents.map((content) => createFragments(content)));
+  return element;
 };
 
 const createPokemonCard = (pokemon) => {
@@ -68,31 +64,45 @@ const createPokemonCard = (pokemon) => {
   const header = [
     "div",
     { class: "header" },
-    createName(pokemon),
+    ["h2", {}, pokemon.pokeName],
     ["div", { class: "types" }, ...createTypes(pokemon)],
   ];
   const stats = ["table", { class: "stats" }, ...createRows(pokemon)];
   return createFragments(["div", { class: "card" }, img, header, stats]);
 };
 
-const createItem = (t, currentType, path) => {
-  return t === currentType
-    ? createFragments(["li", { class: `${t.toLowerCase()}` }, ["a", {
-      href: `/${path}`,
-      style: `color:white`,
-    }, t]])
-    : createFragments(["li", {}, ["a", {
-      href: `/${t === "All" ? "" : t.toLowerCase()}`,
-    }, t]]);
+const getParam = () => {
+  const type = window.location.pathname.split("/").pop();
+  return type === "all" ? "" : type;
 };
 
-const renderPage = (allPokemon, type, cardContainer, sidebarList, types) => {
-  const currentType = type ? capitalizeFirstLetter(type) : "All";
-  sidebarList.append(...types.map((t) => createItem(t, currentType, type)));
-  const pokemon = type
-    ? allPokemon.filter((p) => p.types.includes(currentType))
-    : allPokemon;
-  cardContainer.append(...pokemon.map(createPokemonCard));
+const createItem = (type, currentType, param) =>
+  type === currentType
+    ? createFragments(["li", { class: `${type.toLowerCase()}` }, ["a", {
+      href: `/${param}`,
+      style: `color:white`,
+    }, type]])
+    : createFragments(["li", {}, ["a", {
+      href: `/${type === "All" ? "" : type.toLowerCase()}`,
+    }, type]]);
+
+const getPokemonOnSearch = (allPokemon) => {
+  const searchStr = new URLSearchParams(window.location.search).get("pokemon");
+  return allPokemon
+    .filter((p) => p.pokeName.toLowerCase().includes(searchStr.toLowerCase()));
+};
+
+const getPokemonOnType = (allPokemon, type, currentType) =>
+  type ? allPokemon.filter((p) => p.types.includes(currentType)) : allPokemon;
+
+const renderPage = (cardContainer, sidebarList, allPokemon, types) => {
+  const param = getParam();
+  const type = param ? capitalizeFirstLetter(param) : "All";
+  sidebarList.append(...types.map((t) => createItem(t, type, param)));
+  const filteredPokemon = param === "search"
+    ? getPokemonOnSearch(allPokemon)
+    : getPokemonOnType(allPokemon, param, type);
+  return cardContainer.append(...filteredPokemon.map(createPokemonCard));
 };
 
 window.onload = async () => {
@@ -120,9 +130,7 @@ window.onload = async () => {
     "Water",
   ];
   const allPokemon = await fetchAllPokemon();
-  let type = window.location.pathname.split("/").pop();
-  if (type === "all") type = "";
-  fetch(`/${type}`).then((res) => res.text()).then((_data) =>
-    renderPage(allPokemon, type, cardContainer, sidebarList, types)
-  );
+
+  fetch(window.location.pathname)
+    .then(() => renderPage(cardContainer, sidebarList, allPokemon, types));
 };
